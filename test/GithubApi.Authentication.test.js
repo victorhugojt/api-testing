896b567856b59f04e753f262c46144890b90633c
PSLCOL\vjimenezt <vjimenezt@psl.com.co>
2018-02-26 13:15:15 -0500

    Github 1

---
+++ test/GithubApi.Authentication.test.js
@@ -0,0 +1,20 @@
+const agent = require('superagent-promise')(require('superagent'), Promise);
+const statusCode = require('http-status-codes');
+const { expect } = require('chai');
+
+const urlBase = 'https://api.github.com';
+const githubUserName = 'victorhugojt';
+const repository = 'api-testing';
+
+describe('Github Api Test', () => {
+  describe('Authentication', () => {
+    it('Via OAuth2 Tokens by Header', () =>
+      agent.get(`${urlBase}/repos/${githubUserName}/${repository}`)
+        .auth('token', process.env.ACCESS_TOKEN)
+        .then((response) => {
+         console.log('Token :: '+process.env.ACCESS_TOKEN);
+          expect(response.status).to.equal(statusCode.OK);
+          expect(response.body.description).equal('This is a Workshop about Api Testing in JavaScript');
+        }));
+  });
+});
