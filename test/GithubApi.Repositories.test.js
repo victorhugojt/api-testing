4ec8b1d91c1c3bc5f8b1735493e9c30616994b39
PSLCOL\vjimenezt <vjimenezt@psl.com.co>
2018-02-26 15:37:07 -0500

    getting repo

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -4,6 +4,9 @@ const statusCode = require('http-status-codes');
 const { expect } = require('chai');
 
 describe('Getting repository info', () => {
+
+  var repositoriesURL ;
+  const repoSearch = 'example';
   
   it('Consume GET GitHUB User Service', () => agent.get('https://api.github.com/users/victorhugojt')
    .auth('token', process.env.ACCESS_TOKEN)
@@ -12,13 +15,19 @@ describe('Getting repository info', () => {
     expect(response.body.name).to.equal('Victor Hugo Jimenez Torres');
     expect(response.body.company).to.equal('PSL');
     expect(response.body.location).to.equal('MEDELLIN');
-    
-    console.log(response.body);
 
     repositoriesURL = response.body.repos_url;
 
   }));
 
+  it('Consume GET GitHUB Repositories Service', () => agent.get(repositoriesURL)
+   .auth('token', process.env.ACCESS_TOKEN)
+   .then((response) => {    
+    var repoFound = response.body.find(function(repoSearch) {
+                                    return response.body.name === repoSearch;
+                                  });      
+    console.log(repoFound);
 
+  }));
 
 });
