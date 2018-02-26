ccc8d58b47e944287036b755bca4b5bd30c03caa
PSLCOL\vjimenezt <vjimenezt@psl.com.co>
2018-02-26 16:20:43 -0500

    git repositories

--- test/GithubApi.Repositories.test.js
+++ test/GithubApi.Repositories.test.js
@@ -4,30 +4,28 @@ const statusCode = require('http-status-codes');
 const { expect } = require('chai');
 
 describe('Getting repository info', () => {
+  let repositoriesURL;
+  let repoFound;
+  let repositories;
 
-  var repositoriesURL ;
-  const repoSearch = 'example';
-  
   it('Consume GET GitHUB User Service', () => agent.get('https://api.github.com/users/victorhugojt')
-   .auth('token', process.env.ACCESS_TOKEN)
-   .then((response) => {
-    expect(response.status).to.equal(statusCode.OK);
-    expect(response.body.name).to.equal('Victor Hugo Jimenez Torres');
-    expect(response.body.company).to.equal('PSL');
-    expect(response.body.location).to.equal('MEDELLIN');
+    .auth('token', process.env.ACCESS_TOKEN)
+    .then((response) => {
+      expect(response.status).to.equal(statusCode.OK);
+      expect(response.body.name).to.equal('Victor Hugo Jimenez Torres');
+      expect(response.body.company).to.equal('PSL');
+      expect(response.body.location).to.equal('MEDELLIN');
 
-    repositoriesURL = response.body.repos_url;
-
-  }));
+      repositoriesURL = response.body.repos_url;
+    }));
 
   it('Consume GET GitHUB Repositories Service', () => agent.get(repositoriesURL)
-   .auth('token', process.env.ACCESS_TOKEN)
-   .then((response) => {    
-    var repoFound = response.body.find(function(repoSearch) {
-                                    return response.body.name === repoSearch;
-                                  });      
-    console.log(repoFound);
-
-  }));
-
+    .auth('token', process.env.ACCESS_TOKEN)
+    .then((response) => {
+      expect(response.status).to.equal(statusCode.OK);
+      repositories = response.body;
+      repoFound = repositories.find(element => element.name === 'example');
+      expect(repoFound.full_name).to.equal('victorhugojt/example');
+      expect(repoFound.private).to.equal(false);
+    }));
 });
